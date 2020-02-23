926bc2e787fd481a361593c78a2ffb16eb36f973
htbkoo <h1017760@connect.hku.hk>
2020-02-23 22:31:31 +0800

    added epub loading functionality

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,3 +1,27 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+(function(){
+    const {readEpub} = require('epub-chinese-converter');
+    const path = require('path');
+    const $ = require('jquery');
+
+    $(() => {
+        const $ebookTextArea = $(".ebook-content");
+        const BOOK_URL = path.normalize(`${__dirname}/../resources/book.epub`);
+        $ebookTextArea.text(`Loading ebook from "${BOOK_URL}"`);
+
+        readEpub(BOOK_URL)
+            .then(book => {
+                $ebookTextArea.text(convertBookAsString(book));
+            });
+
+        function convertBookAsString({chapters}) {
+            console.log("at convertBookAsString");
+            return Object.keys(chapters)
+                .map(id => chapters[id].text)
+                .toString();
+        }
+    });
+})();
