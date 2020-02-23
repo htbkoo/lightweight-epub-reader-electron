4b89f8fb81ef7688222a74694bcdf188c2c2951d
htbkoo <h1017760@connect.hku.hk>
2020-02-23 23:06:19 +0800

    (WIP) added usage of electron.remote.dialog to open file

--- src/renderer.ts
+++ src/renderer.ts
@@ -2,13 +2,42 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 
-(function(){
+(function () {
+    console.log("at renderer.ts");
+
     const {readEpub} = require('epub-chinese-converter');
     const path = require('path');
     const $ = require('jquery');
 
+    const $ebookTextArea = $(".ebook-content");
+    const $fileInput = $("#file-path-input");
+
+    $fileInput.on("change", t => {
+        console.log(t);
+        console.log(JSON.stringify(t));
+    });
+
+    $fileInput.on("click", evt => {
+        evt.preventDefault();
+
+        // const {dialog} = require('electron');
+        const {dialog} = require('electron').remote;
+        // console.log(dialog.showOpenDialog({properties: ['openFile', 'multiSelections']}));
+
+        dialog.showOpenDialog({properties: ['openFile',]})
+            .then(({canceled,filePaths }) => {
+                /*
+                opened: {"canceled":true,"filePaths":[]}
+                opened: {"canceled":false,"filePaths":["C:\\Users\\Hey\\Desktop\\PROJECTS\\self\\electron-quick-start-typescript\\resources\\book.epub"]}
+                * */
+                if (!canceled && filePaths){
+                    console.log(`opened: ${JSON.stringify(filePaths)}`);
+
+                }
+            });
+    });
+
     $(() => {
-        const $ebookTextArea = $(".ebook-content");
         const BOOK_URL = path.normalize(`${__dirname}/../resources/book.epub`);
         $ebookTextArea.text(`Loading ebook from "${BOOK_URL}"`);
 
