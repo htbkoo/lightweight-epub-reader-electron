3166bc9bc6314a25149c362e00d14bd5815fc8d5
htbkoo <h1017760@connect.hku.hk>
2020-03-01 14:43:16 +0800

    created `<BookTextArea/>`

---
+++ src/renderer/components/BookTextArea.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import {Book} from "epub-chinese-converter/dist/typings";
+
+const BookTextArea = ({book}: { book: Book.BookWithMeta }) => {
+    const chapters = book.chapters;
+    return (
+        <div>
+            {Object.keys(chapters).map(chapterId => (
+                <div>{chapters[chapterId].text}</div>
+            ))}
+        </div>
+    )
+};
+
+export default BookTextArea;
