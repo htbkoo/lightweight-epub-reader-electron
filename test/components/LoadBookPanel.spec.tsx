99008b551fea0870c835e3e948151c89c042fd5f
htbkoo <h1017760@connect.hku.hk>
2020-03-02 02:02:00 +0800

    added more test coverage; refactored

--- test/components/LoadBookPanel.spec.tsx
+++ test/components/LoadBookPanel.spec.tsx
@@ -1,15 +1,33 @@
 import * as React from 'react';
 import * as renderer from 'react-test-renderer';
 
-import LoadBookPanel from '../../src/renderer/components/LoadBookPanel';
+import LoadBookPanel, {Props} from '../../src/renderer/components/LoadBookPanel';
 import {createBookState} from "../utils/testUtils";
 
 describe('<LoadBookPanel/>', () => {
     it('renders correctly', () => {
         const tree = renderer
-            .create(<LoadBookPanel setFileName={jest.fn()} setBookContent={jest.fn()} notifyLoadingBook={jest.fn()}
-                                   book={createBookState()}/>)
+            .create(createLoadBookPanel())
             .toJSON();
         expect(tree).toMatchSnapshot();
     });
+
+    it('renders when isLoadingBook', () => {
+        const tree = renderer
+            .create(createLoadBookPanel({book: createBookState({isLoadingBook: true})}))
+            .toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+
+    function createLoadBookPanel(overrides: Partial<Props> = {}) {
+        const props: Props = {
+            setFileName: jest.fn(),
+            setBookContent: jest.fn(),
+            notifyLoadingBook: jest.fn(),
+            book: createBookState(),
+            ...overrides
+        };
+
+        return <LoadBookPanel {...props}/>;
+    }
 });
