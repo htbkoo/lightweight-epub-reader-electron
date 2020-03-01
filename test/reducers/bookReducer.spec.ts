cba58f7cb0767b1beb56ae549ea3d0d5a201000c
htbkoo <h1017760@connect.hku.hk>
2020-03-01 22:51:18 +0800

    added test coverage for bookReducer.ts

---
+++ test/reducers/bookReducer.spec.ts
@@ -0,0 +1,18 @@
+import {bookReducer} from "../../src/renderer/reducers/bookReducer";
+import {setBookContent} from "../../src/renderer/actions/bookActions";
+import {Book} from "epub-chinese-converter";
+
+describe('bookReducer', () => {
+    it('renders correctly', () => {
+        // given
+        const bookWithMeta: Book.BookWithMeta = {metadata: {}, chapters: {}};
+        const action = setBookContent(bookWithMeta);
+        const prevState = {};
+
+        // when
+        const newState = bookReducer(prevState, action);
+
+        // then
+        expect(newState.bookWithMeta).toEqual(bookWithMeta);
+    });
+});
