db4dd6811ee16d3cf7b6aa710141ca6eefc1ceb9
htbkoo <h1017760@connect.hku.hk>
2020-03-02 02:51:26 +0800

    updated style; added bookmark bar usage

--- src/renderer/components/Application.tsx
+++ src/renderer/components/Application.tsx
@@ -3,28 +3,36 @@ import * as React from 'react';
 
 import LoadBookPanelContainer from "../containers/LoadBookPanelContainer";
 import BookTextAreaContainer from "../containers/BookTextAreaContainer";
+import BookmarkBarContainer from "../containers/BookmarkBarContainer";
 
 const styles = {
-    "container": {"backgroundColor": "#111", "color": "aliceblue", "min-height": "100%", "overflow-y": "hidden"},
-    "body": {"padding": "25px"},
+    "body": {"backgroundColor": "#111", "color": "aliceblue", "height": "100%", overflowY: "hidden" as any},
+    "padding": {"padding": "1%", height: "100%", boxSizing: "border-box" as any},
+    "container": {"display": "flex" as any, "flexDirection": "column" as any, "height": "100%"},
     "ebook_content": {}
 };
 
 const Application = () => {
     return (
-        <div id="react-mount" className="container-fluid" style={styles.container}>
-            <div style={styles.body}>
-                <div className="row">
-                    <div className="col-md-12">
-                        <LoadBookPanelContainer/>
+        <div id="react-mount" className="container-fluid" style={styles.body}>
+            <div style={styles.padding}>
+                <div style={styles.container}>
+                    <div className="row">
+                        <div className="col-md-12">
+                            <LoadBookPanelContainer/>
+                        </div>
                     </div>
-                </div>
 
-                <div className="row">
-                    <div className="col-md-12">
-                        <div className="panel panel-default">
-                            <div className="ebook-content panel-body" style={styles.ebook_content}>
-                                <BookTextAreaContainer/>
+                    <div className="row" style={{marginBottom: "1%"}}>
+                        <BookmarkBarContainer/>
+                    </div>
+
+                    <div className="row" style={{height: "100%", overflow: "auto", flexGrow: 1}}>
+                        <div className="col-md-12">
+                            <div className="panel panel-default">
+                                <div className="ebook-content panel-body" style={styles.ebook_content}>
+                                    <BookTextAreaContainer/>
+                                </div>
                             </div>
                         </div>
                     </div>
