a0b1e19d996b2ab220daeb86f1ec824b8da8c598
htbkoo <h1017760@connect.hku.hk>
2020-03-22 12:49:32 +0800

    (WIP) migrated to Material-UI styled jss-styling

--- src/renderer/components/Application.tsx
+++ src/renderer/components/Application.tsx
@@ -1,22 +1,27 @@
 import {hot} from 'react-hot-loader/root';
 import * as React from 'react';
+import {createStyles, makeStyles, Theme} from "@material-ui/core/styles";
 
 import LoadBookPanelContainer from "../containers/LoadBookPanelContainer";
 import BookTextAreaContainer from "../containers/BookTextAreaContainer";
 import BookmarkBarContainer from "../containers/BookmarkBarContainer";
 
-const styles = {
-    "body": {"backgroundColor": "#111", "color": "aliceblue", "height": "100%", overflowY: "hidden" as any},
-    "padding": {"padding": "1%", height: "100%", boxSizing: "border-box" as any}, // reference: https://stackoverflow.com/a/41663710
-    "container": {"display": "flex" as any, "flexDirection": "column" as any, "height": "100%"},
-    "ebook_content": {}
-};
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        "body": {backgroundColor: "#111", "color": "aliceblue", "height": "100%", overflowY: "hidden" },
+        "padding": {"padding": "1%", height: "100%", boxSizing: "border-box"}, // reference: https://stackoverflow.com/a/41663710
+        "container": {"display": "flex", "flexDirection": "column", "height": "100%"},
+        "ebook_content": {}
+    }),
+);
 
 const Application = () => {
+    const classes = useStyles();
+
     return (
-        <div id="react-mount" className="container-fluid" style={styles.body}>
-            <div style={styles.padding}>
-                <div style={styles.container}>
+        <div id="react-mount" className={classes.body}>
+            <div className={classes.padding}>
+                <div className={classes.container}>
                     <div className="row">
                         <div className="col-md-12">
                             <LoadBookPanelContainer/>
@@ -30,7 +35,7 @@ const Application = () => {
                     <div className="row" style={{height: "100%", overflow: "auto", flexGrow: 1}}>
                         <div className="col-md-12">
                             <div className="panel panel-default">
-                                <div className="ebook-content panel-body" style={styles.ebook_content}>
+                                <div className={classes.ebook_content}>
                                     <BookTextAreaContainer/>
                                 </div>
                             </div>
