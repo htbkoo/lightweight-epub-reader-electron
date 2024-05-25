0ee865d5bb7300a261c6888e41c289efa40148b5
htbkoo <htbkoo+github@proton.me>
2024-05-25 16:27:27 +0200

    feat: Fix type issue

--- src/renderer/components/LoadBookPanel.tsx
+++ src/renderer/components/LoadBookPanel.tsx
@@ -7,7 +7,7 @@ import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 
 import {BookState} from "../reducers/bookReducer";
-import {ButtonMouseEvent} from '../types';
+import {ButtonMouseEvent} from './types';
 import {AppState} from "../reducers/appReducer";
 
 // const converter = createSimplifiedToTraditionalConverter();
