33eaea96f4a0226a3a205f0b1feb6ee25697fdfa
htbkoo <htbkoo+github@proton.me>
2024-05-25 12:26:19 +0200

    feat(package.json): Fix broken build due to missing optional dependencies `original-fs` and `zipfile`
    Reference:
    1. https://webpack.js.org/plugins/ignore-plugin/
    2. https://github.com/cthackers/adm-zip/issues/242#issuecomment-1024440114

--- webpack.plugins.ts
+++ webpack.plugins.ts
@@ -7,9 +7,19 @@ const ForkTsCheckerWebpackPlugin: typeof IForkTsCheckerWebpackPlugin = require('
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const webpack = require("webpack");
+
 export const plugins = [
   new ForkTsCheckerWebpackPlugin({
     logger: 'webpack-infrastructure',
   }),
   new NodePolyfillPlugin(),
+
+  // To fix the compilation error with the missing optional dependencies `original-fs` and `zipfile`
+  // Reference:
+  // 1. https://webpack.js.org/plugins/ignore-plugin/
+  // 2. https://github.com/cthackers/adm-zip/issues/242#issuecomment-1024440114
+  new webpack.IgnorePlugin({resourceRegExp: /original-fs/}),
+  new webpack.IgnorePlugin({resourceRegExp: /zipfile/}),
 ];
