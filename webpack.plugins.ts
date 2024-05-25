824a6381b810fa8c1259b1cb43fc5c364c572be6
htbkoo <htbkoo+github@proton.me>
2024-05-25 12:14:21 +0200

    feat(package.json): Add `node-polyfill-webpack-plugin` as dependencies to fix build after upgrading to `Webpack 5`
    Reference: https://stackoverflow.com/a/65556946

--- webpack.plugins.ts
+++ webpack.plugins.ts
@@ -3,8 +3,13 @@ import type IForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const ForkTsCheckerWebpackPlugin: typeof IForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
+// reference: https://stackoverflow.com/a/65556946
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
+
 export const plugins = [
   new ForkTsCheckerWebpackPlugin({
     logger: 'webpack-infrastructure',
   }),
+  new NodePolyfillPlugin(),
 ];
